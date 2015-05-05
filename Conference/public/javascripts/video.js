1bfbb563c73dcc8cd0cc8f50dc1e894dd9e432dc
kaicheng feng <kaicheng.feng@columbia.edu>
2015-05-04 21:23:24 -0400

    shit

---
+++ Conference/public/javascripts/video.js
@@ -0,0 +1,98 @@
+    function generateVideoFrame(peerId){
+        var box = document.createElement("div");
+        box.setAttribute("class","thumbnail");
+        var box2 = document.createElement("div");
+        box2.setAttribute("class","col-lg-6");
+        box2.appendChild(box);
+        box2.setAttribute("id","video:"+peerId);
+        document.getElementById("videoFrames").appendChild(box2);
+        return box;
+    }
+    var webrtc = new SimpleWebRTC({
+        localVideoEl: 'localVideo',
+        remoteVideoEl: '',
+        autoRequestMedia: true,
+        debug: false,
+        detectSpeakingEvents: true,
+        autoAdjustMic: false
+    });
+    webrtc.on('readyToCall', function () {
+                var mainV = document.getElementById("mainVideo");
+                console.log(mainV.parentNode.offsetWidth);
+                clickActive("videoLabel");
+                sendVideoRequest();
+                mainV.style.width=0.975*mainV.parentNode.offsetWidth+"px";
+                mainV.style.height=0.7*mainV.parentNode.offsetWidth+"px";
+            });
+    webrtc.on('localStream', function (stream) {
+    });
+    // we did not get access to the camera
+    webrtc.on('localMediaError', function (err) {
+    });
+    webrtc.on('videoAdded', function (video, peer) {
+        console.log('video added', peer);
+        var remotes = generateVideoFrame(webrtc.getDomId(peer));
+        if (remotes) {
+            var container = document.createElement('div');
+            //container.style.textAlign="center";
+            container.className = 'videoContainer';
+            container.id = 'container_' + webrtc.getDomId(peer);
+            container.appendChild(video);
+
+            // suppress contextmenu
+            video.oncontextmenu = function () { return false; };
+
+            // resize the video on click
+            // show the remote volume
+            var vol = document.createElement('meter');
+            vol.id = 'volume_' + peer.id;
+            vol.className = 'volume';
+            vol.min = -45;
+            vol.max = -20;
+            vol.low = -40;
+            vol.high = -25;
+            container.appendChild(vol);
+            
+            // show the ice connection state
+            if (peer && peer.pc) {
+                var connstate = document.createElement('div');
+                connstate.className = 'connectionstate';
+                container.appendChild(connstate);
+                peer.pc.on('iceConnectionStateChange', function (event) {
+                    switch (peer.pc.iceConnectionState) {
+                    case 'checking': 
+                        connstate.innerText = 'Connecting to peer...';
+                        break;
+                    case 'connected':
+                    case 'completed': // on caller side
+                        //$(vol).show();
+                        connstate.innerText = 'Connection established.';
+                        break;
+                    case 'disconnected':
+                        //connstate.innerText = 'Disconnected.';
+                        break;
+                    case 'failed':
+                        connstate.innerText = 'Connection failed.';
+                        break;
+                    case 'closed':
+                        connstate.innerText = 'Connection closed.';
+                        console.log("video:"+webrtc.getDomId(peer));
+                        var currFrame = document.getElementById("video:"+webrtc.getDomId(peer));
+                        currFrame.parentNode.removeChild(currFrame);
+                        break;
+                    }
+                });
+            }
+            remotes.appendChild(container);
+            container.style.width=0.975*container.parentNode.offsetWidth+"px";
+            container.style.height=0.7*container.parentNode.offsetWidth+"px";
+        }
+    });
+    webrtc.on('videoRemoved', function (video, peer) {
+          console.log('video removed ', peer);
+          var remotes = document.getElementById('remotes');
+          var el = document.getElementById(peer ? 'container_' + webrtc.getDomId(peer) : 'localScreenContainer');
+          if (remotes && el) {
+              remotes.removeChild(el);
+          }
+      });
---
+++ Conference/routes/index.js
@@ -0,0 +1,169 @@
+var express = require('express');
+var router = express.Router();
+var um = require('userManagment');
+
+
+/* GET home page. */
+
+
+router.get('/', function(req, res, next) {
+  if(req.session.userName){
+  	um.login(req.session.userName,req.session.password,function(){
+  		res.render('join', { title: 'Welcome', roomNumber:req.session.roomNumber ,userName:req.session.userName,logedIn: "yes",footer: '' });
+	},function(){
+		res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+	});
+  }else{
+  	res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+  }
+});
+
+
+//===========upload============
+router.get('/upload', function(req,res,next){
+	if(req.session.roomNumber!=undefined && req.session.userName!=undefined){
+			res.render('page_after_login/upload', {roomNumber: req.session.roomNumber, userName: req.session.userName});
+		}else{
+			res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+		}
+});
+//=============================
+
+
+router.get('/profile', function(req, res, next) {
+   um.login(req.session.userName,req.session.password,function(){
+  		um.getProfile(req.session.userName,function(lastName,firstName,suffix,description){
+  			res.render('profile', {title: 'profile', lastName: lastName, firstName: firstName, suffix: suffix, description: description, userName:req.session.userName, logedIn: "yes", footer: ''});
+  		},function(){
+  			res.send("can't get your profile!");
+  		});
+	},function(){
+		res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+	});
+});
+
+
+router.get('/logout', function(req, res, next) {
+   req.session.destroy();
+   res.redirect('/');
+});
+
+
+router.get('/back', function(req, res, next) {
+	um.login(req.session.userName,req.session.password,function(){
+		res.render('join', { title: 'Welcome', roomNumber:'' ,userName:req.session.userName,logedIn: "yes", userId:req.session.userId,footer: '', roomPassword: req.session.roomPassword, alert: " " });
+	}, function(){
+		res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+	});
+});
+
+
+router.post('/signUp',function(req,res,next){
+	console.log(req.body.userId+req.body.password);
+	um.createUser(req.body.userId,req.body.password,function(){
+		console.log("good");
+		res.redirect(307,'/login');
+	},function(){
+		res.redirect('/');
+	});
+});
+
+
+router.get('/dashboard', function(req, res, next) {
+   um.login(req.session.userName,req.session.password,function(){
+  		if(req.session.roomNumber!=undefined){
+  			res.render('page_after_login/body', {userName:req.session.userName,roomNumber: req.session.roomNumber});
+  		}else{
+  			res.render('join', { title: 'Welcome', userName:req.session.userName, logedIn: "yes", userId:req.session.userName,footer: '' });
+  		}
+	},function(){
+		res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+	});
+});
+
+
+router.get('/changeProfile',function(req,res,next){
+	um.setProfile(req.param('userId'),req.param('lastName'),req.param('firstName'),req.param('suffix'),req.param('description'),function(){
+		res.redirect('/profile');
+	},function(){
+		res.redirect('/profile');
+	});
+});
+
+
+router.post('/login', function(req, res, next) {
+	var userId = req.body.userId;
+	var password = req.body.password;
+	var sess;
+	um.login(userId,password,function(){
+		sess = req.session;
+		sess.userName=userId;
+		sess.password=password;
+		res.render('join', { title: 'Welcome', roomNumber:'' ,userName:req.session.userName,logedIn: "yes", userId:userId,footer: '', roomPassword: req.session.roomPassword, alert: " " });
+	},function(){
+		res.send("fail!");
+	});
+  	//res.send(userId+password);
+});
+
+
+router.post('/joinRoomRequest', function(req, res, next) {
+	um.login(req.session.userName,req.session.password,function(){	
+	//on login success	
+		var roomNumber = req.body.roomNumber;
+		var roomPassword = req.body.roomPassword;
+		var requestType = req.body.requestType;
+		req.session.roomNumber=roomNumber;
+		req.session.roomPassword=roomPassword;
+		var db = require('dbHelper');
+
+		console.log("request type = "+requestType);
+
+		if (requestType==="join"){
+			//verify room credential function
+			var checkRoomCredential = function(roomNumber, password, onSuccess, onFailure){
+				db.query("select * from room where roomnumber = '"+roomNumber+"' AND password = '"+roomPassword+"'",function(error,results){
+					if(error || results.length==0){
+						onFailure();
+					}else{
+						onSuccess();
+					}
+				});
+			}
+			//verify room credential
+		  	checkRoomCredential(roomNumber, roomPassword,
+		  	function(){	//room credential good
+		  		res.render('page_after_login/body', {userName:req.session.userName,roomNumber: roomNumber, roomPassword: req.session.roomPassword});
+			},
+			function(){	//room credential bad
+				res.render('join', { title: 'Welcome', roomNumber:'' ,userName:req.session.userName,logedIn: "yes", userId:req.session.userId,footer: '', roomPassword: req.session.roomPassword, alert: "room does not exist or incorrect password." });
+			});
+		}else{
+			//verify existance function
+			var checkRoomExistance = function(roomNumber, onSuccess, onFailure){
+				db.query("select * from room where roomnumber = '"+roomNumber+"'",function(error,results){
+					if(error || results.length==0){
+						onFailure();
+					}else{
+						onSuccess();
+					}
+				});
+			}
+			//verify existance 
+		  	checkRoomExistance(roomNumber,
+		  	function(){	//room already existed
+				res.render('join', { title: 'Welcome', roomNumber:'' ,userName:req.session.userName,logedIn: "yes", userId:req.session.userId,footer: '', roomPassword: req.session.roomPassword, alert: "room with this room number already exists." });
+			},
+			function(){	//room not exist
+				db.query("insert into room(roomNumber,password) values('"+roomNumber+"','"+roomPassword+"')",function(error,results){});
+		  		res.render('page_after_login/body', {userName:req.session.userName,roomNumber: roomNumber, roomPassword: req.session.roomPassword});
+			});
+		}
+
+	},function(){	//on login fail
+		res.render('index', { title: 'Welcome', logedIn: "no", footer: '' });
+	});
+});
+
+
+module.exports = router;
